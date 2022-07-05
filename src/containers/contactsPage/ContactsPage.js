16329dd25c457624ed3e4c2b61b40704154c2928
Pedro Revez da Silva <pedro.revez.silva@gmail.com>
2022-07-05 22:55:32 +0100

    Add props to contactsPage

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {ContactForm} from "../../components/contactForm/ContactForm";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = ({contacts, addContact}) => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [duplicate, setDuplicate] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    if(!duplicate) {
+      addContact(name, phone, email);
+      setName("");
+      setPhone("");
+      setEmail("");
+    }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  useEffect(() => {
+    const isDuplicated = () => {
+      const found = contacts.find(contact => contact.name === name);
+      if(found !== undefined) {
+        return true;
+      }
+      return false;
+    };
+
+    if(isDuplicated()) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [name, contacts, duplicate]);
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>
+          Add Contact~
+          {duplicate ? "-Name already exists" : ""}
+          </h2> 
+
       </section>
       <hr />
       <section>
